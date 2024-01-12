4bc3ebd646ecdbb1517b5295a14fc988297db789
VGiudicelli <vincent.giudicelli@ensg.eu>
2024-01-12 09:47:46 +0100

    add initials files

---
+++ src/test/java/tsi/ensg/prjEval/PrjEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package tsi.ensg.prjEval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
