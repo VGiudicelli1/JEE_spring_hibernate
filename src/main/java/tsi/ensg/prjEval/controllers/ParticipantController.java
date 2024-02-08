45e61ff092913379141ade5a3ca8e24114a9afdd
VGiudicelli <vincent.giudicelli@ensg.eu>
2024-02-08 18:47:45 +0100

    add edit participant

--- src/main/java/tsi/ensg/prjEval/controllers/ParticipantController.java
+++ src/main/java/tsi/ensg/prjEval/controllers/ParticipantController.java
@@ -20,8 +20,14 @@ public class ParticipantController {
     @Autowired
     private ParticipantService participantService;
 
+    @GetMapping("/participants")
+    public String list(Model model) {
+        model.addAttribute("participants", participantService.findAll());
+        return "participant/list";
+    }
+
     @GetMapping("/participants/{id_participant}")
-    public String getEventWithId(@PathVariable("id_participant") long id_participant, Model model) {
+    public String info(@PathVariable("id_participant") long id_participant, Model model) {
         Participant participant = participantService.findById(id_participant).orElse(null);
         System.out.println(participant);
         model.addAttribute("participant", participant);
@@ -29,23 +35,49 @@ public class ParticipantController {
     }
 
     @GetMapping("/participants/new")
-    public String newParticipant(Model model) {
+    public String add(Model model) {
         model.addAttribute("participant", new Participant());
-        return "participant/add";
-    }
-
-    @GetMapping("/participants")
-    public String list(Model model) {
-        model.addAttribute("participants", participantService.findAll());
-        return "participant/list";
+        return "participant/save";
     }
 
     @PostMapping("/participants/new")
-    public String addEvent(@Validated Participant participant, BindingResult result, Model model) {
+    public String add(@Validated Participant participant, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            return "participant/add";
+            return "participant/save";
         }
         participantService.save(participant);
         return "redirect:/participants/" + participant.getId();
     }
+
+    @GetMapping("/participants/edit/{id_participant}")
+    public String edit(@PathVariable long id_participant, Model model) {
+        Optional<Participant> participant = participantService.findById(id_participant);
+        if (participant.isEmpty()) {
+            return "redirect:/participants/" + id_participant;
+        } else {
+            model.addAttribute("participant", participant.get());
+            return "participant/save";
+        }
+    }
+
+
+    @PostMapping("/participants/edit/{id_participant}")
+    public String edit(@PathVariable long id_participant, @Validated Participant participant, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "participant/save/";
+        }
+        Participant participantOriginal = participantService.findById(id_participant).orElse(null);
+        if (participantOriginal == null) {
+            // une erreur s'est produite
+            return "redirect:/participants";
+        }
+        participant.setId(id_participant);
+
+        participant.setEvents(participantOriginal.getEvents()); // restore participant
+
+        participantService.save(participant);
+        return "redirect:/participants/"+id_participant;
+    }
+
+
 }
